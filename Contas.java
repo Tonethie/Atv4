b8c37977c20360be93e76968729f53eebc5964a5
Tonethie <luiz_fcarvalho@hotmail.com>
2017-05-23 22:00:33 -0300

    Update and rename Data.java to Contas.java

---
+++ Contas.java
@@ -0,0 +1,14 @@
+package Conta;
+
+public class Contas {
+	
+	public static void main(String args[]) {
+		ContaCorrente c = new ContaCorrente();
+		OP op1 = new OP(1000);
+		c.addOP(op1);
+			
+	}
+	public void Extrato(){
+		
+	}
+}
